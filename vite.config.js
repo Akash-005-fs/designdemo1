e38adbc0b2905a6eed7c6f7abe590f2dbdcd78cf
Akash-005-fs <akashpradeepktm@gmail.com>
2026-06-13 10:14:04 +0530

    Add .gitignore and clean repo

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
-  base: '/designdemo/'   // repo name here
+  base: '/designdemo/'
 })
